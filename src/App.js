0e6c64467cbf8f0d52818e7843732f61322f9d41
Fraser Solomon <fmsolomon1@gmail.com>
2018-07-05 19:09:36 +1000

    majority of app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
+// Import React and Component
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import CSS from App.css
+import './styles/styles.css'
+// Import the Today component to be used below
+import Today from './components/Today'
+// Import the History component to be used below
+import History from './components/History'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="">
+          <div className="topheader">
+              <header className="container">
+                  <nav className="navbar">
+                      <div className="navbar-brand">
+                          <span className="navbar-item">cryptoMate</span>
+                      </div>
+                      <div className="navbar-end">
+                          <a className="navbar-item" href="https://pusher.com" target="_blank" rel="noopener noreferrer">about</a>
+                      </div>
+                  </nav>
+              </header>
+          </div>
+          <section className="results--section">
+              <div className="container">
+                  <h1>cryptoMate<br></br> live BTC, ETH and LTC.</h1>
+              </div>
+              <div className="results--section__inner">
+                  <Today />
+                  <History />
+              </div>
+          </section>
       </div>
     );
   }
